39d7b4058c522efdadd70c491d47c62613af4afc
S0fiaMedina <151967289+S0fiaMedina@users.noreply.github.com>
2024-07-07 16:08:18 -0500

    feat: :tada: initial commit
    This commit...
    - adds the very initial structure for this new project, so its very suceptible to changes

---
+++ src/main/java/com/example/theleague/TheleagueApplication.java
@@ -0,0 +1,13 @@
+package com.example.theleague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheleagueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheleagueApplication.class, args);
+	}
+
+}
